d51b5ff3412fb36b4d6c803c91496b6f8cd583f2
Yuplawxen <yuftiyannaaqurrota@gmail.com>
2026-03-05 10:58:09 +0700

    Menambahkan loop kedua dengan i dimulai dari 0

--- BubbleShort.cpp
+++ BubbleShort.cpp
@@ -14,5 +14,14 @@ void input(){
         else {
             cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
         }
-    }    
+    }   
+    cout << endl;
+    cout << "====================" << endl;
+    cout << "Masukan Elemen Array" << endl;
+    cout << "====================" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "Data ke-" << (i + i) << ":";
+        cin >> arr[i];
+    }
 }
